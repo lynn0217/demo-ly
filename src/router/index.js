a7fa1530f1a0412825c348fb2bd573f9d7e0d6c8
lynn0217 <2579234709@qq.com>
2022-01-17 14:38:13 +0800

    第一次提交

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: () => import('../views/acount/Login.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/acount/Login.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="login">
+  <el-button type="warning">Warning</el-button>
+</div>
+</template>
+
+<style lang="less" scoped>
+  @import "./styles.less";
+</style>
